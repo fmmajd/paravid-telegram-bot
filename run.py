dd5e9e2be82068a63a24dabf05b3d3e4052d4659
Fatemeh Majd <fmmajd452@gmail.com>
2018-03-24 01:08:28 +0430

    ran first command and sent first message in bot

--- run.py
+++ run.py
@@ -1,1 +1,18 @@
-print('hello world!')
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+
+updater = Updater('568438669:AAGANxKfG1HjvhfOJpWQewljIjKTXMqbd4E')
+
+
+def start(bot, update):
+    chat_id = update.message.chat_id
+    bot.sendMessage(chat_id, 'hello dear user!')
+
+
+start_command = CommandHandler('start', start)
+
+
+updater.dispatcher.add_handler(start_command)
+
+updater.start_polling()
+updater.idle()  # for windows, to exit with ctrl-c
